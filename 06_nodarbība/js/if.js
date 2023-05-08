52e4ba67631fa637107c06765acd85efee241d4e
Lizzely <130855715+Lizzely@users.noreply.github.com>
2023-05-08 23:08:33 +0300

    20230508_23_08

--- 06_nodarbība/js/if.js
+++ 06_nodarbība/js/if.js
@@ -1,10 +1,8 @@
-if (new Date().getHours() < 20) {
+if (new Date().getHours() < 18) {
     document.getElementById("demo").innerHTML = "Good day!";
-    document.getElementById("demo").innerHTML = document.getElementById("demo").innerHTML + "Good day one more time!";
+    document.getElementById("demo").innerHTML =
+        document.getElementById("demo").innerHTML + "Good day one more time!";
 }
 
-
-if (new Date().getHours() < 20)
-console.log("Good day!");
+if (new Date().getHours() < 18) console.log("Good day!");
 console.log("Good day one more time!");
-    
