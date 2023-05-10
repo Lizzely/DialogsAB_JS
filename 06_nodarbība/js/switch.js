b40a35e220e98d54174d484c70decd79151c8d3d
Lizzely <130855715+Lizzely@users.noreply.github.com>
2023-05-10 23:50:22 +0300

    20230510_23_50

--- 06_nodarbība/js/switch.js
+++ 06_nodarbība/js/switch.js
@@ -1,6 +1,7 @@
 let day;
+let day_ = new Date().getDay();
 
-switch (new Date().getDay()) {
+switch (day_) {
   case 0:
     day = "Sunday";
     break;
@@ -22,8 +23,32 @@ switch (new Date().getDay()) {
   case 6:
     day = "Saturday";
     console.log(day);
-  case 6:
+    //day_ =7;
+    break;
+      case 7:
     day = "Saturdayyyyyy";
     console.log(day);
 }
 document.getElementById("demo_switch").innerHTML = "Today is " + day;
+
+
+switch (day_) {
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  default: 
+   text = "Looking forward to the Working day!";
+  }
+   document.getElementById("demo_switch").innerHTML = document.getElementById("demo_switch").innerHTML + text;
