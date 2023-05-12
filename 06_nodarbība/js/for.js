433c107ac724c9130ca4cdbf38d079aa02f5b4dc
Lizzely <130855715+Lizzely@users.noreply.github.com>
2023-05-12 18:45:01 +0300

    20230512_18_45

--- 06_nodarbība/js/for.js
+++ 06_nodarbība/js/for.js
@@ -5,8 +5,11 @@ for (let i = 0; i < cars.length; i++) {
     // text += cars[i] + "<br>"; // -= *= /= %=
     console.log(i);
     console.log("text mainīgā saturs pirms izmaiņām:" + "\n" + text);
+    if (i==2) break;
     text = text + cars[i] + "<br>";
-    console.log("text mainīgā saturs pirms izmaiņām:" + "\n" + text);
+    console.log("text mainīgā saturs pēc izmaiņām:" + "\n" + text + "\n\n");
 }
+
+// console.log(i);
 
 document.getElementById("demo_for").innerHTML = text;
