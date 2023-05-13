f27d43a763568b3e4ba80a325c6e941f8589519d
Lizzely <130855715+Lizzely@users.noreply.github.com>
2023-05-13 19:54:38 +0300

    20230513_19_54

--- 08_nodarbība/js/objects.js
+++ 08_nodarbība/js/objects.js
@@ -3,3 +3,13 @@ const array_example = ['a', 'b'];
 const function_example = function name_for_internal_use(x) { return x * x; };
 
 const car = { type: "Fiat", model: "500", color: "white" };
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    }
+};
+
